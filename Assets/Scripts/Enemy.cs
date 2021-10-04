1bb66b1c6c66caa816859064aa1e785c2a0d1a78
James da Silva <james.dasilva1111@gmail.com>
2021-10-05 03:09:37 +1100

    Added a wave spawning system

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float m_MovementSpeed = 1.0f;
+
+    public event Action OnDestinationReached;
+
+    Vector2[] m_Waypoints;
+
+    Vector3 m_MovementDirection;
+
+    int m_CurrentWaypoint;
+
+    bool m_IsMoving;
+
+    public void Initialize(Vector2[] a_Waypoints)
+    {
+        m_Waypoints = a_Waypoints;
+
+        m_CurrentWaypoint = 0;
+
+        m_MovementDirection = (new Vector3(a_Waypoints[0].x, a_Waypoints[0].y, transform.position.z) - transform.position).normalized;
+
+        m_IsMoving = true;
+    }
+
+    void Update()
+    {
+        if (m_IsMoving)
+        {
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        float _FrameMovement = m_MovementSpeed * Time.deltaTime;
+        float _DistanceToWaypoint = Vector2.Distance(transform.position, m_Waypoints[m_CurrentWaypoint]);
+
+        // If the travel distance would allow the enemy to reach the waypoint
+        while (_FrameMovement >= _DistanceToWaypoint)
+        {
+            _FrameMovement -= _DistanceToWaypoint;
+
+            transform.position = m_Waypoints[m_CurrentWaypoint];
+
+            m_CurrentWaypoint++;
+
+            if (m_CurrentWaypoint == m_Waypoints.Length)
+            {
+                m_IsMoving = false;
+
+                DestinationReached();
+
+                return;
+            }
+
+            _DistanceToWaypoint = Vector2.Distance(transform.position, m_Waypoints[m_CurrentWaypoint]);
+
+            m_MovementDirection = (new Vector3(m_Waypoints[m_CurrentWaypoint].x, m_Waypoints[m_CurrentWaypoint].y, transform.position.z) - transform.position).normalized;
+        }
+
+        transform.position += m_MovementDirection * _FrameMovement;
+    }
+
+    void DestinationReached()
+    {
+        OnDestinationReached?.Invoke();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class WaveManager : MonoBehaviour
+{
+    [SerializeField] Vector2[] m_Waypoints;
+    [SerializeField] Wave[] m_Waves;
+
+    int m_CurrentWave = -1;
+
+#if UNITY_EDITOR
+    void OnDrawGizmosSelected()
+    {
+        GUIStyle _Style = new GUIStyle();
+
+        _Style.alignment = TextAnchor.UpperCenter;
+        _Style.fontSize = 20;
+
+        Gizmos.color = Color.yellow;
+
+        Handles.Label(transform.position + new Vector3(0, 0.5f, 0), "Spawn", _Style);
+
+        Gizmos.DrawSphere(transform.position, 0.1f);
+
+        Vector3 _WaypointPos = transform.position;
+        Vector3 _PreviousWaypointPos;
+
+        for (int i = 0; i < m_Waypoints.Length; i++)
+        {
+            _PreviousWaypointPos = _WaypointPos;
+            _WaypointPos = new Vector3(m_Waypoints[i].x, m_Waypoints[i].y, transform.position.z);
+
+            Handles.Label(_WaypointPos + new Vector3(0, 0.5f, 0), $"{i}", _Style);
+
+            Gizmos.DrawSphere(_WaypointPos, 0.1f);
+            Gizmos.DrawLine(_PreviousWaypointPos, _WaypointPos);
+        }
+    }
+#endif
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnNextWave();
+        }
+    }
+
+    void SpawnNextWave()
+    {
+        m_CurrentWave++;
+
+        Debug.Log($"Wave {m_CurrentWave}");
+
+        StartCoroutine(SpawnWaveCoroutine(m_Waves[m_CurrentWave]));
+    }
+
+    IEnumerator SpawnWaveCoroutine(Wave a_Wave)
+    {
+        for (int i = 0; i < a_Wave.EnemyGroups.Length; i++)
+        {
+            yield return new WaitForSeconds(a_Wave.EnemyGroups[i].SpawnDelay);
+
+            for (int j = 0; j < a_Wave.EnemyGroups[i].Amount; j++)
+            {
+                Enemy _Enemy = Instantiate(a_Wave.EnemyGroups[i].Enemy, transform.position, Quaternion.identity);
+
+                _Enemy.Initialize(m_Waypoints);
+
+                if (j != a_Wave.EnemyGroups[i].Amount - 1)
+                {
+                    yield return new WaitForSeconds(a_Wave.EnemyGroups[i].SpawnInterval);
+                }
+            }
+        }
+    }
+
+    [Serializable]
+    public class Wave
+    {
+        public EnemyGroup[] EnemyGroups;
+    }
+
+    [Serializable]
+    public class EnemyGroup
+    {
+        public Enemy Enemy;
+        public int Amount;
+        public float SpawnInterval; // Time between spawns within this group
+        public float SpawnDelay; // Time before this group starts spawning
+    }
+}
