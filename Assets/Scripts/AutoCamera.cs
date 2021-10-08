a6193ef579a148bfca8551ed6056ffa5c8cd7a8a
James da Silva <james.dasilva1111@gmail.com>
2021-10-09 03:53:04 +1100

    Fixed AutoCamera not scaling properly with greater distances, made it much more snappy

--- Assets/Scripts/AutoCamera.cs
+++ Assets/Scripts/AutoCamera.cs
@@ -7,8 +7,10 @@ public class AutoCamera : MonoBehaviour
     [SerializeField] Camera m_Camera;
     [SerializeField] WaveManager m_WaveManager;
     [SerializeField] float m_MinZoom = 5.0f;
+    [SerializeField] float m_ViewPadding = 1.0f;
     [SerializeField] float m_MapWidth;
     [SerializeField] float m_MapHeight;
+    [SerializeField] float m_TrackSpeed = 4.0f;
 
     Vector3 m_Position;
     float m_Zoom = 5.0f;
@@ -76,12 +78,12 @@ public class AutoCamera : MonoBehaviour
                 {
                     Vector2 _CenterPos = new Vector2((_MinX + _MaxX) / 2, (_MinY + _MaxY) / 2);
 
-                    float _FurthestXDistance = Mathf.Abs(_MaxX - _CenterPos.x);
-                    float _FurthestYDistance = Mathf.Abs(_MaxY - _CenterPos.y);
+                    float _FurthestXDistance = Mathf.Abs(_MaxX - _CenterPos.x) + m_ViewPadding;
+                    float _FurthestYDistance = Mathf.Abs(_MaxY - _CenterPos.y) + m_ViewPadding;
 
                     // Compensate for aspect ratio
-                    // If two enemies are split on the Y axis, we have a lot less screen real estate to work with, so we need to zoom out sooner than on the X axis
-                    _FurthestYDistance *= (float)Screen.width / Screen.height;
+                    // If two enemies are split on the X axis, we have a lot more screen real estate to work with, so we need to zoom out later than on the Y axis
+                    _FurthestXDistance *= (float)Screen.height / Screen.width;
 
                     float _FurthestDistance;
 
@@ -100,19 +102,26 @@ public class AutoCamera : MonoBehaviour
 
                     float _ZoomDifference = m_Zoom - m_Camera.orthographicSize;
 
-                    m_Camera.orthographicSize += _ZoomDifference * Time.deltaTime / 2;
-
                     float _XDifference = m_Position.x - transform.position.x;
                     float _YDifference = m_Position.y - transform.position.y;
 
-                    m_Camera.orthographicSize += _ZoomDifference * Time.deltaTime / 2;
+                    m_Camera.orthographicSize += _ZoomDifference * Time.deltaTime * m_TrackSpeed;
 
                     transform.position += new Vector3
                     (
-                        _XDifference * Time.deltaTime,
-                        _YDifference * Time.deltaTime,
+                        _XDifference * Time.deltaTime * m_TrackSpeed,
+                        _YDifference * Time.deltaTime * m_TrackSpeed,
                         0
                     );
+
+                    //m_Camera.orthographicSize = m_Zoom;
+
+                    //transform.position += new Vector3
+                    //(
+                        //_XDifference,
+                        //_YDifference,
+                        //0
+                    //);
                 }
             }
         }
