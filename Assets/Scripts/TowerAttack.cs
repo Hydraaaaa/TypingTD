e6ef1dc0ddcecd7f2b0ece666902033bb4733337
James da Silva <james.dasilva1111@gmail.com>
2021-10-11 05:01:03 +1100

    Made basic tower target the closest enemy, instead of all enemies

--- Assets/Scripts/TowerAttack.cs
+++ Assets/Scripts/TowerAttack.cs
@@ -4,38 +4,50 @@ using UnityEngine;
 
 public class TowerAttack : MonoBehaviour
 {
+    public bool TowerPlaced;
+
     [SerializeField] int range;
     [SerializeField] float fireRate;
+
     private float time = 0f;
-    public bool towerPlaced = false;
-    // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
-        
+        TowerPlaced = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            towerPlaced = true;
+            TowerPlaced = true;
         }
-        else if (towerPlaced == true)
+        else if (TowerPlaced == true)
         {
             time += Time.deltaTime;
             if (time > fireRate)
             {
-                if (Enemy.Enemies != null)
+                if (Enemy.Enemies != null &&
+                    Enemy.Enemies.Count > 0)
                 {
-                    for (int i = 0; i < Enemy.Enemies.Count; i++)
+                    Debug.Log($"!! TEST");
+                    Enemy _ClosestEnemy = Enemy.Enemies[0];
+                    float _ClosestDistance = Vector3.Distance(transform.position, Enemy.Enemies[0].Position);
+
+                    for (int i = 1; i < Enemy.Enemies.Count; i++)
                     {
-                        if (Vector3.Distance(transform.position, Enemy.Enemies[i].Position) < range)
+                        float _Distance = Vector3.Distance(transform.position, Enemy.Enemies[i].Position);
+
+                        if (_Distance < _ClosestDistance)
                         {
-                            Enemy.Enemies[i].Health -= 1;
+                            _ClosestEnemy = Enemy.Enemies[i];
+                            _ClosestDistance = _Distance;
                         }
                     }
+
+                    _ClosestEnemy.Health -= 1;
                 }
+
                 time -= fireRate;
             }
         }
