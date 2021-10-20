efac18b4388480e96ee3b0d9c45c39387b2fedf6
groobob <91916133+groobob@users.noreply.github.com>
2021-10-20 09:51:19 -0400

    instantiated tower can now attack (sort of)
    help

---
+++ Assets/Scripts/TowerAttackSniper.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerAttackSniper : MonoBehaviour
+{
+    public bool TowerPlaced;
+
+    [SerializeField] int range;
+    [SerializeField] float fireRate;
+    [SerializeField] GameObject shotEffect;
+    [SerializeField] int damage;
+    [SerializeField] float attackRate;
+
+    private float time = 0f;
+
+    void Awake()
+    {
+        TowerPlaced = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            TowerPlaced = true;
+        }
+        else if (TowerPlaced == true)
+        {
+            time += Time.deltaTime;
+            if (time > fireRate)
+            {
+                if (Enemy.Enemies != null &&
+                    Enemy.Enemies.Count > 0)
+                {
+                    Enemy _ClosestEnemy = Enemy.Enemies[0];
+                    float _ClosestDistance = Vector3.Distance(transform.position, Enemy.Enemies[0].Position);
+
+                    for (int i = 1; i < Enemy.Enemies.Count; i++)
+                    {
+                        float _Distance = Vector3.Distance(transform.position, Enemy.Enemies[i].Position);
+
+                        if (_Distance < _ClosestDistance)
+                        {
+                            _ClosestEnemy = Enemy.Enemies[i];
+                            _ClosestDistance = _Distance;
+                        }
+                    }
+                    if (_ClosestDistance < range)
+                    {
+                        // me trying to make the tower attack rapidly (damage) amount of times so it looks cooler
+                        for (int i = 0; i <= damage; i++)
+                        {
+                            if (time > attackRate)
+                            {
+                                Destroy(Instantiate(shotEffect, transform.position, Quaternion.Euler(new Vector3(90, 0, 0))), 3f);
+                                _ClosestEnemy.Health -= 1;
+                                time -= attackRate;
+                            }
+                        }
+
+                    }
+                }
+                time -= fireRate;
+            }
+        }
+    }
+}
