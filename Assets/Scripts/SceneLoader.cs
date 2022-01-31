c04cbb057833e4646aba31f9119807efde8b9c51
groobob <91916133+groobob@users.noreply.github.com>
2022-01-31 17:10:06 -0500

    added a bunch of new scenes
    a start to different menus

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,11 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-
     public void LoadNextScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+    public void LoadLevelSelectScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void LoadSettingsScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void LoadAboutScene()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void LoadMapOne()
+    {
+        SceneManager.LoadScene(4);
     }
     public void LoadStartScene()
     {
