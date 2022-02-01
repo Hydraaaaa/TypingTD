6c05b0990d4e0b8328885d22de133d8eddf25b73
James da Silva <james.dasilva1111@gmail.com>
2022-02-01 11:45:17 +1100

    Added play/pause functionality to animation scripts

--- Assets/Scripts/SpriteAnimation.cs
+++ Assets/Scripts/SpriteAnimation.cs
@@ -6,33 +6,100 @@ using UnityEngine;
 public class SpriteAnimation : MonoBehaviour
 {
     [SerializeField] SpriteRenderer m_SpriteRenderer;
-    [SerializeField] float m_Interval = 0.2f;
     [SerializeField] Sprite[] m_Sprites;
+
+    [Space]
+
+    public float Interval = 0.2f;
+    [SerializeField] bool m_PlayOnAwake;
+    public bool Loop;
 
     float m_CurrentInterval = 0.0f;
     int m_Index = 0;
+    bool m_IsPlaying = false;
 
     void Reset()
     {
         m_SpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
+    void Awake()
+    {
+        m_IsPlaying = m_PlayOnAwake;
+
+        if (m_Sprites.Length > 0)
+        {
+            m_SpriteRenderer.sprite = m_Sprites[0];
+        }
+    }
+
     void Update()
     {
-        m_CurrentInterval += Time.deltaTime;
-
-        if (m_CurrentInterval >= m_Interval)
+        if (m_Sprites.Length == 0)
         {
-            m_CurrentInterval -= m_Interval;
-
-            m_Index++;
-
-            if (m_Index >= m_Sprites.Length)
+            if (m_IsPlaying)
             {
-                m_Index = 0;
+                Debug.LogError("Attempted to play animation without any sprites");
             }
 
-            m_SpriteRenderer.sprite = m_Sprites[m_Index];
+            m_IsPlaying = false;
+            m_Index = 0;
+            m_CurrentInterval = 0.0f;
         }
+
+        if (m_IsPlaying)
+        {
+            m_CurrentInterval += Time.deltaTime;
+
+            if (m_CurrentInterval >= Interval)
+            {
+                m_Index++;
+
+                m_CurrentInterval -= Interval;
+
+                if (m_Index >= m_Sprites.Length)
+                {
+                    m_Index = 0;
+
+                    if (!Loop)
+                    {
+                        m_IsPlaying = false;
+                        m_CurrentInterval = 0.0f;
+                    }
+                    else
+                    {
+                        m_SpriteRenderer.sprite = m_Sprites[m_Index];
+                    }
+                }
+                else
+                {
+                    m_SpriteRenderer.sprite = m_Sprites[m_Index];
+                }
+            }
+        }
+    }
+
+    public void Play()
+    {
+        m_IsPlaying = true;
+    }
+
+    public void Pause()
+    {
+        m_IsPlaying = false;
+    }
+
+    public void Stop()
+    {
+        m_IsPlaying = false;
+
+        m_Index = 0;
+        m_CurrentInterval = 0.0f;
+    }
+
+    public void SetFrame(int a_Frame)
+    {
+        m_Index = Mathf.Clamp(a_Frame, 0, m_Sprites.Length - 1);
+        m_CurrentInterval = 0.0f;
     }
 }
